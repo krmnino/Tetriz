619308e06f66271fa07d501ef7f4cd67968de108
krmnino <kurt.manrique.n@gmail.com>
2020-03-13 13:32:39 -0400

    working on populate()

--- src/Queue.java
+++ src/Queue.java
@@ -10,12 +10,25 @@ public class Queue {
 		this.size = 0;
 	}
 	public void populate() {
+		Node curr;
+		this.head = new Node(new Block(1, 1));
+		this.head.get_data().set_shape_type();
+		this.head.get_data().set_block();
+		this.size++;
+		curr = this.head.get_next();
+		while(size < MAX_SIZE) {
+			curr = new Node(new Block(1, 1));
+			curr.get_data().set_shape_type();
+			curr.get_data().set_block();
+			curr = this.head.get_next();
+			this.size++;
+		}
 		
 	}
 	public Block dequeue_shape() {
 		Block out = this.head.get_data();
 		this.head = this.head.get_next();
-		Block new_tail = new Block();
+		Block new_tail = new Block(1, 1);
 		new_tail.set_shape_type();
 		new_tail.set_block();
 		this.tail = new Node(new_tail);
