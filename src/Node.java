1de2626bef0c53670ed0aa1c656b39a35268c0bc
krmnino <kurt.manrique.n@gmail.com>
2020-03-30 11:04:21 -0400

    Added comments in Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,25 +1,25 @@
-public class Node {
-	private Block data;
+public class Node { //singly link list of Nodes containing Block objects
+	private Block data; 
 	private Node next;
 	
-	public Node(Block data_) {
-		this.data = data_;
-		this.next = null;
+	public Node(Block data_) { //constructor method Node class
+		this.data = data_; //set data from parameter value
+		this.next = null; //set next Node to null
 	}
 	
-	public void set_data(Block data_) {
+	public void set_data(Block data_) { //set data in Node
 		this.data = data_;
 	}
 	
-	public Block get_data() {
+	public Block get_data() { //get data stored in Node
 		return this.data;
 	}
 	
-	public void set_next(Node next_) {
+	public void set_next(Node next_) { //set next Node reference
 		this.next = next_;
 	}
 	
-	public Node get_next() {
+	public Node get_next() { //get reference to next Node
 		return this.next;
 	}
 }
